4fce7559e1cc6509d6929de138378812dd6ef4f4
Priyanka V <iampriyankavivek@gmail.com>
2025-10-05 23:01:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,28 +5,27 @@ import torch
 
 st.title("Image Captioning")
 
-# --- Load BLIP model ---
+# Loading BLIP model 
 @st.cache_resource
 def load_model():
     return BLIPCaptioner()
 
 captioner = load_model()
 
-# --- Streamlit UI ---
 uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
 decoding = st.selectbox("Decoding Strategy", ["greedy", "beam", "nucleus"])
 max_length = st.slider("Max caption length", 5, 50, 20)
 
 if uploaded_file:
-    # Display the uploaded image
+  e
     image = Image.open(uploaded_file).convert("RGB")
     st.image(image, caption="Uploaded Image", use_container_width=True)
 
     # Generate caption button
     if st.button(" Generate Caption"):
         with st.spinner("Generating caption..."):
-            # Pass uploaded_file directly to BLIP
+            
             caption = captioner.generate_caption(
                 uploaded_file,
                 prompt=None,  # no prompt
@@ -35,3 +34,4 @@ if uploaded_file:
             )
         st.success("Caption generated!")
         st.markdown(f"**Generated Caption:** {caption}")
+
